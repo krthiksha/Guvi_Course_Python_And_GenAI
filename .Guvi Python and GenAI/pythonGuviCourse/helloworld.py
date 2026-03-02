01cfbc251497e9e471c5fddb39c90c4485480301
krthiksha <krithiksha21@gmail.com>
2026-03-02 22:31:53 +0000

    learning

---
+++ .Guvi Python and GenAI/pythonGuviCourse/helloworld.py
@@ -0,0 +1,21 @@
+# print statement
+print("hello world")
+
+# addition of two numbers
+a = 10
+b = 20
+c = a + b
+print("The sum of a and b is:", c)
+
+""" to view the BYTE CODE for the above code, we can use the dis module 
+run the following command in the terminal:
+python -m dis <ur_file_name.py>
+
+
+python code  -->   interpreter [complier --> byte code  --> machine code]  --> output
+
+"""
+
+
+help(print)
+
